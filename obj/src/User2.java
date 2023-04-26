2698cf9ec23463ae0fd549086036ba30bb9b5afa
SeaStar <1811230375@qq.com>
2023-04-26 16:57:32 +0800

    Change

--- obj/src/User2.java
+++ obj/src/User2.java
@@ -4,6 +4,10 @@ public class User2 {
     String pwd;
     public User2(){
     }
+    public User2(int id, String name, String pwd){
+        this(id, name);
+        this.pwd = pwd;
+    }
 
     public User2(int id,String name){
         this.id =id;
@@ -16,6 +20,8 @@ public class User2 {
     public static void main(String[] args) {
         User2 u = new User2(100,"海星");
         u.show();
+        User2 u0 = new User2(101,"happy","123456");
+        u0.show();
 
     }
 
