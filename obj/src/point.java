3777296e95985dabb67ddb62a068c407a98fd3a5
SeaStar <1811230375@qq.com>
2023-04-26 15:44:12 +0800

    main

---
+++ obj/src/point.java
@@ -0,0 +1,16 @@
+/**
+ * 描述2维点
+ */
+public class point {
+    double x;
+    double y;
+
+    public point(double _x, double _y){
+    x =_x;
+    y = _y;
+
+    }
+    public static void main(String[] args) {
+    point P1 = new point(3.0,4.0);
+    }
+}
