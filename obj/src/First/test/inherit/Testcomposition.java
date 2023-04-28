fc3a4ba6c3f736b556fa53e157df768216718eba
SeaStar <1811230375@qq.com>
2023-04-28 11:39:54 +0800

    Change

---
+++ obj/src/First/test/inherit/Testcomposition.java
@@ -0,0 +1,44 @@
+package First.test.inherit;
+
+public class Testcomposition {
+    public static void main(String[] args) {
+        STU stu1 = new STU("Happy",175.0,"java");
+        STU stu2 = new STU();
+        stu1.study();
+        stu2.study();
+        Computer computer = new Computer();
+        computer.cpu.calcuclate();
+        computer.memory.store();
+
+    }
+}
+class STU{
+    person person = new person();
+    String mojar;
+    public void study(){
+        System.out.println(this.person.name + " is Studying");
+    }
+public STU(){
+
+    }
+    public STU(String name,double height,String mojar){
+        this.person.name = name;
+        this.person.height = height;
+        this.mojar = mojar;
+
+    }
+}
+class Memory{
+    public void store(){
+        System.out.println("Memory.store");
+    }
+}
+class CPU{
+    public void calcuclate(){
+        System.out.println("CPU.calculate");
+    }
+}
+class Computer{
+Memory memory = new Memory();
+CPU cpu = new CPU();
+}
