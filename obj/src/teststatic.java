1f656394e6a044ff78ee95cb8c9e5da32e9a016a
SeaStar <1811230375@qq.com>
2023-04-28 09:19:42 +0800

    Change

--- obj/src/teststatic.java
+++ obj/src/teststatic.java
@@ -1,2 +1,17 @@
 public class teststatic {
+    int id;
+    static String Company = "Happy";
+     static void printcompany(){
+         System.out.println(Company);
+     }
+     void login(){
+         System.out.println("Login Successful");
+     }
+
+    public static void main(String[] args) {
+        teststatic.printcompany();
+
+        teststatic t1 = new teststatic();
+        t1.login();
+    }
 }
